88c0ced34b05ef2ba5c7b91dbcd97ac24322e216
Anand Gavai <anand.gavai@gmail.com>
2016-04-21 14:39:22 +0200

    added decorators for flask
    This was the crucial step to make sure the webservice is working correctly.

--- callRFromWebService.py
+++ callRFromWebService.py
@@ -1,24 +1,68 @@
-import rpy2
+#import rpy2
 from flask import Flask
 from flask import request
 import json 
 from numpy import *
-from rpy2.robjects.numpy2ri import numpy2ri
+#from rpy2.robjects.numpy2ri import numpy2ri
 import scipy as sp
 from pandas import *
 import pickle
-from rpy2.robjects.packages import importr
-import rpy2.robjects as ro
-import rpy2.rinterface as rinterface
-import pandas.rpy.common as com
-from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+#from rpy2.robjects.packages import importr
+#import rpy2.robjects as ro
+#import rpy2.rinterface as rinterface
+#import pandas.rpy.common as com
+#from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
 import subprocess
 import sys
-from rpy2.robjects import pandas2ri
-pandas2ri.activate()
+#from rpy2.robjects import pandas2ri
+#pandas2ri.activate()
 
 
 app = Flask(__name__)
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 @app.route('/')
 def run_script():
@@ -48,18 +92,20 @@ def api_formTestScores():
         return t
 """
 
-@app.route('/formTestScores',methods=['GET','POST'])
+@app.route('/formTestScores',methods=['GET','POST','OPTIONS'])
+@crossdomain(origin='*',headers='Content-type')
 def api_formTestScores():
-    if request.headers['Content-Type']=='application/json':
+    #if request.headers['Content-Type']=='application/json':
         #jsonlite=importr('jsonlite')
+	print "-->"+request.data+"<--"
         myPyJSON=request.data
-        print myPyJSON
-        p = subprocess.Popen(["Rscript", "functionforandi1.R"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        #print myPyJSON
+        p = subprocess.Popen(["Rscript", "functionforandi.R"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         t = p.communicate(myPyJSON)
-        print t
+        #print t
         return t
 
 
 if __name__ == '__main__':
     app.debug=True
-    app.run()
+    app.run(host='0.0.0.0',port=5000)
